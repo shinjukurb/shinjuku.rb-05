be690f82aa57776d3cd3e95985b5ded9805cef73
Shigenobu Nishikawa <snishikawa@aiming-inc.com>
2012-05-23 20:30:57 +0900

    Added Nabeatsu case with 13.

--- nabeatsu.rb
+++ nabeatsu.rb
@@ -3,7 +3,8 @@ class Nabeatsu
   end
 
   def say(number)
-    return "hoge" if number == 3 
+    return "hoge" if number == 3
+    return "hoge" if number == 13
     number.to_s
   end
 end
--- nabeatsu_spec.rb
+++ nabeatsu_spec.rb
@@ -1,8 +1,17 @@
 require './nabeatsu'
-describe "Nabeatsu" do
-  it "should say hoge on three " do
-    someone = Nabeatsu.new
-    someone.say(1).should eq "1"
-    someone.say(3).should eq "hoge"
+describe Nabeatsu do
+  describe "say" do
+    before do
+      @someone = Nabeatsu.new
+    end
+    it "should say hoge given 3 " do
+      @someone.say(3).should eq "hoge"
+    end
+    it "should say 1 given 1 " do
+      @someone.say(1).should eq "1"
+    end
+    it "should say hoge given 13 " do
+      @someone.say(13).should eq "hoge"
+    end
   end
 end
